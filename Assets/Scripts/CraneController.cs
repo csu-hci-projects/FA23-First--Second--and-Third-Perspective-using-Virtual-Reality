0d547f54dbf570439313dba6f036b8f05c05b237
Tanmay Prabhu Naidu <tanmay.naidu@colostate.edu>
2023-11-07 16:22:16 -0700

    Crane Test

---
+++ Assets/Scripts/CraneController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void RotateLeft()
+    {
+
+    }
+    public void RotateRight()
+    {
+
+    }
+    public void MoveForward()
+    {
+
+    }
+    public void MoveBackward()
+    {
+
+    }
+    public void MoveUp()
+    {
+
+    }
+    public void MoveDown()
+    {
+
+    }
+    public void Grab()
+    {
+
+    }
+    public void UnGrab()
+    {
+
+    }
+}
